5a60ea1b9ad9c09e9462b4567f81e9f9c6590143
mrchenliang <c.liang@ualberta.ca>
2020-06-07 13:55:41 -0400

    setting up checkout page with adding, removing and deleting items

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -7,3 +7,11 @@ export const addItem = item => ({
     type: CartActionTypes.ADD_ITEM,
     payload: item
 })
+export const removeItem = item => ({
+    type: CartActionTypes.REMOVE_ITEM,
+    payload: item
+})
+export const deleteItem = item => ({
+    type: CartActionTypes.DELETE_ITEM,
+    payload: item
+})
