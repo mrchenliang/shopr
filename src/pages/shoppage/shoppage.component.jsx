cf04f63f5b8e1ca4dd41330e84f8638f9905d763
mrchenliang <c.liang@ualberta.ca>
2020-06-07 22:37:05 -0400

    setting up page for advanced routing

--- src/pages/shoppage/shoppage.component.jsx
+++ src/pages/shoppage/shoppage.component.jsx
@@ -1,21 +1,11 @@
 import React from "react";
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import CollectionsOveriew from "../../components/collections-overview/collections-overview.component";
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
-
-const ShopPage = ({ collections })=> (
+const ShopPage = ()=> (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    <CollectionsOveriew />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-})
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
