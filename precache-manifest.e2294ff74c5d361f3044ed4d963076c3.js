03298fadddb3866a370ca57b2002a166e08ed0a9
mrchenliang <c.liang@ualberta.ca>
2020-06-07 22:17:03 -0400

    Updates

---
+++ precache-manifest.e2294ff74c5d361f3044ed4d963076c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a56a23a4a9a290d7965e83bfdcf5ca3b",
+    "url": "/shopr/index.html"
+  },
+  {
+    "revision": "7c814c5e5cff8d25cbd7",
+    "url": "/shopr/static/css/main.3573191f.chunk.css"
+  },
+  {
+    "revision": "15d7e65a3d856c050436",
+    "url": "/shopr/static/js/2.956e6cbc.chunk.js"
+  },
+  {
+    "revision": "7c814c5e5cff8d25cbd7",
+    "url": "/shopr/static/js/main.5e021a5e.chunk.js"
+  },
+  {
+    "revision": "398b52eeaec76efef722",
+    "url": "/shopr/static/js/runtime~main.39f20c3d.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/shopr/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/shopr/static/media/shopping-bag.30f07071.svg"
+  }
+]);
